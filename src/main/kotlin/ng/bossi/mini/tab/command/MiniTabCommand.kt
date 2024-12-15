df6443ca8e3e9e4f7897120a21fa933b9fad7ee0
Max Bossing <info@maxbossing.de>
2024-12-15 01:07:23 +0100

    feature: Allow non-OP players to execute /tab info, print info with only /tab

--- src/main/kotlin/ng/bossi/mini/tab/command/MiniTabCommand.kt
+++ src/main/kotlin/ng/bossi/mini/tab/command/MiniTabCommand.kt
@@ -2,6 +2,7 @@
 
 package ng.bossi.mini.tab.command
 
+import dev.jorel.commandapi.executors.CommandArguments
 import dev.jorel.commandapi.kotlindsl.anyExecutor
 import dev.jorel.commandapi.kotlindsl.commandTree
 import dev.jorel.commandapi.kotlindsl.literalArgument
@@ -12,11 +13,34 @@ import ng.bossi.minekraft.text.plus
 import ng.bossi.minekraft.text.text
 import ng.bossi.mini.tab.MiniTab
 import ng.bossi.mini.tab.prefix
+import org.bukkit.command.CommandSender
 
 object MiniTabCommand {
+    private val sendInfo = { sender: CommandSender, _: CommandArguments ->
+        sender.sendMessage(
+            prefix + text("Running MiniTab version ") + text(
+                MinekraftInstance.pluginMeta.version,
+                Kolors.CORNFLOWERBLUE
+            )
+        )
+        sender.sendMessage(prefix + text("TextManager: ${MiniTab.textManager.name}"))
+        sender.sendMessage(
+            prefix + text("MiniTab is part of ", italic = true) + text(
+                "MiniPaper",
+                Kolors.ROYALPURPLE
+            ).clickEvent(ClickEvent.openUrl("https://mini.bossi.ng"))
+        )
+    }
+
     init {
         commandTree("tab") {
-            withPermission("minitab.admin")
+            anyExecutor(sendInfo)
+
+            literalArgument("info") {
+                anyExecutor(sendInfo)
+            }
+
+            withPermission("mini.tab.manage")
             literalArgument("reload") {
                 anyExecutor { sender, _ ->
                     MiniTab.reload()
@@ -38,24 +62,6 @@ object MiniTabCommand {
                     if (MiniTab.textManager.stop())
                         sender.sendMessage(prefix + text("MiniTab stopped"))
                     else sender.sendMessage(prefix + text("MiniTab already stopped!", Kolors.RED))
-                }
-            }
-
-            literalArgument("info") {
-                anyExecutor { sender, _ ->
-                    sender.sendMessage(
-                        prefix + text("Running MiniTab version ") + text(
-                            MinekraftInstance.pluginMeta.version,
-                            Kolors.CORNFLOWERBLUE
-                        )
-                    )
-                    sender.sendMessage(prefix + text("TextManager: ${MiniTab.textManager.name}"))
-                    sender.sendMessage(
-                        prefix + text("MiniTab is part of ", italic = true) + text(
-                            "MiniPaper",
-                            Kolors.ROYALPURPLE
-                        ).clickEvent(ClickEvent.openUrl("https://mini.bossi.ng"))
-                    )
                 }
             }
         }
